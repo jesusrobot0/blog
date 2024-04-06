8595c01d07d477a8e73b02bbc6bb7070d9e69e54
jesusrobot0 <jesusrobot9000@icloud.com>
2024-04-06 18:22:53 -0500

    Add strapi-client connection test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-export default function Home() {
+import { PublicationsResponse } from "./interface";
+
+async function getPublications(): Promise<PublicationsResponse> {
+  const endpoint =
+    "https://blog-api-dashboard-production.up.railway.app/api/publications";
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  return data;
+}
+
+export default async function Home() {
+  const publications = await getPublications();
+  console.log(publications.data);
+
   return (
     <>
-      <h1>Hello World</h1>
+      {publications.data.map((publication) => (
+        <h1>{publication.attributes.title}</h1>
+      ))}
     </>
   );
 }
