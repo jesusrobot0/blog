d4576e48b1790bbeb98bf818dd11ea81e665f1b0
jesusrobot0 <jesusrobot9000@icloud.com>
2024-04-08 16:17:41 -0500

    Remove strapi-client connection test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-import { PublicationsResponse } from "./interface";
-
-async function getPublications(): Promise<PublicationsResponse> {
-  const endpoint =
-    "https://blog-api-dashboard-production.up.railway.app/api/publications";
-  const response = await fetch(endpoint);
-  const data = await response.json();
-
-  return data;
-}
-
 export default async function Home() {
-  const publications = await getPublications();
-  console.log(publications.data);
-
-  return (
-    <>
-      {publications.data.map((publication) => (
-        <h1 key={publication.attributes.slug}>
-          {publication.attributes.title}
-        </h1>
-      ))}
-    </>
-  );
+  return <h1>Jesús Velasco</h1>;
 }
