7dafb6956f9f6d689cc24de95598c7408a1db72f
jesusrobot0 <jesusrobot9000@icloud.com>
2024-04-08 17:00:04 -0500

    Add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      background: "#503d34",
+      headingText: "#fff",
+      subHeading: "#121212",
+      bodyFont: "#121212",
     },
   },
   plugins: [],
