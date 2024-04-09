43aff9134be3461fd304d4f7a3fa3fef40ec869a
jesusrobot0 <jesusrobot9000@icloud.com>
2024-04-08 19:31:11 -0500

    Add logo font font

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,3 +1,5 @@
 import { EB_Garamond } from "next/font/google";
+import { Chivo } from "next/font/google";
 
 export const primaryFont = EB_Garamond({ subsets: ["latin"] });
+export const LogoFont = Chivo({ subsets: ["latin"] });
