60dd8f2e77033ee34acd26c9cfed6cd3c64693e0
jesusrobot0 <jesusrobot9000@icloud.com>
2024-04-26 02:00:28 -0500

    Fixes contrast problems in text

--- src/ui/components/footer/Footer.tsx
+++ src/ui/components/footer/Footer.tsx
@@ -7,15 +7,15 @@ export function Footer() {
       <Wrapper>
         <div className="flex flex-col gap-[60px] min-[1200px]:gap-[76px]">
           <div className="flex flex-col gap-10 sm:flex-row sm:justify-between sm:items-center">
-            <h3 className="flex flex-col text-[64px] leading-[80px] min-[1200px]:text-[130px] min-[1200px]:leading-[155px]">
+            <p className="flex flex-col text-[64px] leading-[80px] min-[1200px]:text-[130px] min-[1200px]:leading-[155px]">
               <span>Let's start</span>
               <span>something </span>
               <span>new together</span>
-            </h3>
+            </p>
             <CircularButtom />
           </div>
           <FooterNav />
-          <div className="flex flex-col gap-8 text-[18px] text-[#B39B94] min-[1200px]:flex-row min-[1200px]:justify-between">
+          <div className="flex flex-col gap-8 text-[18px] text-[#c5aba3] min-[1200px]:flex-row min-[1200px]:justify-between">
             <div className="flex flex-col gap-8 sm:flex-row">
               <Link href="#">© 2022 Keeper, All rights reserved</Link>
               <Link href="#">Privacy Policy</Link>
