635198b97d4d6b3b3bf07db6e1f3eae8f13ff826
jesusrobot0 <jesusrobot9000@icloud.com>
2024-04-25 21:44:19 -0500

    Update CircularButtom size on large screens

--- src/ui/components/circular-buttom/CircularButtom.tsx
+++ src/ui/components/circular-buttom/CircularButtom.tsx
@@ -6,7 +6,7 @@ export function CircularButtom() {
       href="#"
       className="min-w-[241px] max-w-[241px] h-[240px] flex justify-center items-center"
     >
-      <div className="min-w-[170px] max-w-[170px] h-[169px] flex items-center justify-center text-background rounded-full bg-[#AEB946] hover:bg-[#AD9C94] min-[1200px]:min-w-[200px] min-[1200px]:max-w-[200px] min-[1200px]:h-[200px]">
+      <div className="min-w-[170px] max-w-[170px] h-[169px] flex items-center justify-center text-background rounded-full bg-[#AEB946] hover:bg-[#AD9C94] min-[1200px]:min-w-[240px] min-[1200px]:max-w-[240px] min-[1200px]:h-[240px]">
         GET IN TOUCH
       </div>
     </Link>
