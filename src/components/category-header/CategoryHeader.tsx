3931157920b9d7c63551913b7572ec1ff0b3a378
jesusrobot0 <jesusrobot9000@icloud.com>
2024-05-16 01:39:25 -0500

    Fixes props writing by switching to camelCase

--- src/components/category-header/CategoryHeader.tsx
+++ src/components/category-header/CategoryHeader.tsx
@@ -29,9 +29,9 @@ export function CategoryHeader({ category, postCount }: Props) {
             viewBox="0 0 24 24"
             fill="none"
             stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             className=""
           >
             <path d="m6 9 6 6 6-6" />
