c20c23fbe7b9494a158168e4c802d407ae9b179a
jesusrobot0 <jesusrobot9000@icloud.com>
2024-05-19 12:59:04 -0500

    Update layout types

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,16 +5,16 @@ import { primaryFont } from "@/config/fonts";
 import { Footer, Navbar } from "@/components";
 import "./globals.css";
 
+interface Props {
+  readonly children: React.ReactNode;
+}
+
 export const metadata: Metadata = {
   title: "Jesús Velasco",
   description: "lorem ipsum dolor sit amet",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Props) {
   return (
     <html lang="es">
       <body className={`${primaryFont.className} text-[#121212]`}>
