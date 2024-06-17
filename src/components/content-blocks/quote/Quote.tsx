a41e30fbc81561e0f8d5b633cf208f24659e34ca
jesusrobot0 <jesusrobot9000@icloud.com>
2024-06-17 18:07:33 -0500

    Update in the Quoute component to define the height and width of the icon

--- src/components/content-blocks/quote/Quote.tsx
+++ src/components/content-blocks/quote/Quote.tsx
@@ -6,7 +6,11 @@ export function Quote({ text = "" }: Props) {
 
   return (
     <blockquote className="flex flex-col gap-[20px] sm:flex-row sm:items-start sm:gap-0">
-      <img src="/icons/quote.svg" alt="Quote icon" className="w-[80px]" />
+      <img
+        src="/icons/quote.svg"
+        alt="Quote icon"
+        className="w-[80px] h-[80px]"
+      />
       <span className="text-[32px] tracking-[-0.18px] leading-[42px] sm:text-[48px] sm:tracking-[-.64px] sm:leading-[64px] lg:w-2/3">
         {text}
       </span>
